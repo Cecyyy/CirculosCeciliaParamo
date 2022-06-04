1ae8bde3a7792373b965ec6e8ac7b5ef637602e0
Cecyyy <cecyparamo94@gmail.com>
2022-06-04 15:19:36 -0500

    creacion de los getters y setters

--- src/Circulo.java
+++ src/Circulo.java
@@ -2,5 +2,11 @@ public class Circulo {
 
         private double radio;
 
+    public double getRadio() {
+        return radio;
+    }
 
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
 }
